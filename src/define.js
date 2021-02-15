e9bf941fc97fd8ece33a0c2d2127d195025a73c0
Bruno Foggia <bruno.foggia@cbyk.com.br>
2021-02-15 15:53:30 -0300

    abstraction of load view routines

--- src/define.js
+++ src/define.js
@@ -18,56 +18,56 @@ bbxf.view = bbx.view.extend();
 bbxf.model.prototype['formatOnToJSON'] = true;
 
 // locator
-for(let x in locator['model']) {
-	bbxf.model[x] = locator['model'][x];
+for (let x in locator['model']) {
+    bbxf.model[x] = locator['model'][x];
 }
 bbxf.locate = _.bind(bbxf.locator.find, bbxf.locator);
 bbxf.iModel = _.bind(_.partial(bbxf.locator.find, 'iModel'), bbxf.locator);
 
-for(let x in locator['model_prototype']) {
-	bbxf.model.prototype[x] = locator['model_prototype'][x];
+for (let x in locator['model_prototype']) {
+    bbxf.model.prototype[x] = locator['model_prototype'][x];
 }
 
-for(let x in locator['collection']) {
-	bbxf.collection[x] = locator['collection'][x];
+for (let x in locator['collection']) {
+    bbxf.collection[x] = locator['collection'][x];
 }
 
-for(let x in locator['collection_prototype']) {
-	bbxf.collection.prototype[x] = locator['collection_prototype'][x];
+for (let x in locator['collection_prototype']) {
+    bbxf.collection.prototype[x] = locator['collection_prototype'][x];
 }
 
 // router
 bbxf.router = router;
 
 // security
-bbxf.view.Shield = function(options={}) {
+bbxf.view.Shield = function (options = {}) {
     router.setShield(this.prototype.className, options);
     return this;
-}
+};
 
 // state
-for(let x in state['model']) {
-	bbxf.model[x] = state['model'][x];
+for (let x in state['model']) {
+    bbxf.model[x] = state['model'][x];
 }
 
-for(let x in state['model_prototype']) {
-	bbxf.model.prototype[x] = state['model_prototype'][x];
+for (let x in state['model_prototype']) {
+    bbxf.model.prototype[x] = state['model_prototype'][x];
 }
 
-for(let x in state['collection']) {
-	bbxf.collection[x] = state['collection'][x];
+for (let x in state['collection']) {
+    bbxf.collection[x] = state['collection'][x];
 }
 
-for(let x in state['collection_prototype']) {
-	bbxf.collection.prototype[x] = state['collection_prototype'][x];
+for (let x in state['collection_prototype']) {
+    bbxf.collection.prototype[x] = state['collection_prototype'][x];
 }
 
-for(let x in state['view']) {
-	bbxf.view[x] = state['view'][x];
+for (let x in state['view']) {
+    bbxf.view[x] = state['view'][x];
 }
 
-for(let x in state['view_prototype']) {
-	bbxf.view.prototype[x] = state['view_prototype'][x];
+for (let x in state['view_prototype']) {
+    bbxf.view.prototype[x] = state['view_prototype'][x];
 }
 
 // validation
@@ -77,5 +77,15 @@ bbxf.vmodel = bbxf.model.extend(_.extend({}, bbxf.validation));
 
 // utils
 bbxf.utils = utils;
+
+// load view
+bbxf.loadView = async function (viewName, callback) {
+    if (typeof bbxf.importView !== 'function') {
+        throw new Error('define importView first');
+    }
+
+    await bbxf.importView(viewName);
+    typeof callback === 'function' && callback(viewName);
+};
 
 export default bbxf;
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,32 @@
 /**
- * 
+ *
  */
 import _ from 'underscore-99xp';
 import front from 'front-99xp';
 
 var utils = _.extend({}, front.utils);
 
-utils.showView = async function(view, id=null, callback=null) {
+utils.showView = async function (view, id = null, callback = null) {
     var viewClass = view;
-    if(typeof view === 'string') {
+    if (typeof view === 'string') {
         viewClass = await this.getView(view);
     }
-    
-    var viewObj = typeof view === 'string' ? (new viewClass({id: id})) : view,
-            App = front.locator.getItem('iApp');
-    
+
+    var viewObj = typeof view === 'string' ? new viewClass({ id: id }) : view,
+        App = front.locator.getItem('iApp');
+
     typeof callback === 'function' && (viewObj.callback = callback);
     App.showContent(viewObj);
     return viewObj;
+};
+
+utils.getView = async function (view) {
+    if (!front.locator.getListItem('view', view)) {
+        await front.importView(view);
+    }
+    var viewClass = front.locator.getListItem('view', view);
+
+    return viewClass;
 };
 
 export default utils;
