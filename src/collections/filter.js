1e3e92b31955b93eec0a7932959e6a89dd9f1cc8
Bruno Foggia <bruno.foggia@cbyk.com.br>
2021-03-10 12:59:26 -0300

    added parameter to decide when sending or not empty filter values

--- src/collections/filter.js
+++ src/collections/filter.js
@@ -2,13 +2,28 @@ import _ from 'underscore-99xp';
 import bbxf from '../define';
 
 export default bbxf.collection.extend({
+    sendEmptyFilter: {},
     sync(method, model, options) {
         if (this.filterOnServer) {
             method = 'create';
-            options.attrs = this.filter.toJSON();
+            options.attrs = this.prepareFilterValues(this.filter.toJSON());
             options.url = this.url();
         }
 
         _.bind(bbxf.collection.prototype.sync, this)(method, model, options);
+    },
+    prepareFilterValues(d) {
+        var j = {};
+        if (_.size(d) > 0) {
+            for (var x in d) {
+                if (!(d[x] + '').trim() == '') {
+                    j[x] = d[x];
+                } else if (this.filter.sendEmptyFilter[x] !== false) {
+                    j[x] = '';
+                }
+            }
+        }
+
+        return j;
     },
 });
